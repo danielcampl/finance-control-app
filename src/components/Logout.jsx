6cdef895447de5dbd5dbc14bbb4c06f1e2a5a742
daniel <danielcampelo159@gmail.com>
2025-09-02 08:47:46 -0300

    Finalizando tailwindcss de sidebar e acrescentando funcao para interagir com ela.

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,16 +1,20 @@
 import { auth } from '../firebaseConfig';
 import { signOut } from 'firebase/auth';
+import { Button } from "@material-tailwind/react";
 
 export default function Logout({ user }) {
     return (
-        <section>
-            <h1>Olá, {user.displayName}</h1>
-            <button
+        <section className='w-full flex flex-col justify-start gap-2'>
+            <div className='flex justify-start gap-2 items-center justify-start'>
+                <img src={user.photoURL} alt="userPhoto" className='w-8 h-8' />
+                <span className='text-sm font-semibold'>{user.displayName}</span>
+            </div>
+            <Button
+                className="w-44 h-10 font-sans font-semibold text-white bg-[#212529] hover:bg-[#343a40]"
                 onClick={() => signOut(auth)}
-                className=''
             >
                 Sair
-            </button>
+            </Button>
         </section>
     )
 }
